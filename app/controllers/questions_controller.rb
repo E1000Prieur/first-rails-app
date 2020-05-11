7feb5fff9c1b48a01b86c833b61680897af56cdc
Emile Prieur <emileprieur@gmail.com>
2020-05-11 14:55:59 +0200

    I'm going to wokr right now git add app config

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @asker = params[:answer]
+    if @asker == "I'm going to work"
+      @answer = "Great"
+    elsif @asker.include?("?")
+      @answer = "Silly question, get dress and go to work !!"
+    else
+      @answer = "I don't care !! Go to work right now !"
+    end
+  end
+
 end
